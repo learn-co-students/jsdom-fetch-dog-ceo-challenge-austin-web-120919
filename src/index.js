0f0be95a281b3a9a99aec6af0a13cd9a3f9d9107
Chance Mims <chance@Chances-MacBook-Air.local>
2020-01-29 15:20:56 -0600

    challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () =>{
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+        .then(resp => resp.json())
+        .then(json =>{
+             console.log(json);
+            const imageDiv = document.getElementById("dog-image-container");
+            json.message.forEach(function(imgLink){
+                imgRef = document.createElement('img');
+                imageDiv.appendChild(imgRef);
+                imgRef.setAttribute('src', imgLink);
+            });
+        }
+        );
+
+       
+
+})
